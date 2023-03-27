f1d1bc3efcd9095bb85b231f2518528ee894b8bc
Grandman7777 <kokaiya7777@gmail.com>
2023-03-27 22:25:13 +0300

    Sort

--- src/components/AlohaDashboard/AlohaDashboard.js
+++ src/components/AlohaDashboard/AlohaDashboard.js
@@ -1,37 +1,60 @@
 import React from 'react';
 import Aloha from "../Aloha/Aloha";
 
-const usersArray = [
-    {
-        firstName: 'Tony',
-        lastName: 'Stark'
-    },
-    {
-        firstName: 'bruce',
-        lastName: 'Branner'
-    },
-    {
-        firstName: 'Thonr',
-        lastName: 'Odinson'
-    },
-    {
-        firstName: 'Peter',
-        lastName: 'Parker'
-    },
-    {
-        firstName: 'Natasha',
-        lastName: 'Romanof'
-    }
-]
-
 class AlohaDashboard extends React.Component {
+     constructor (props) {
+        super(props);e
+        this.state = {
+        usersArray : [
+            {
+                firstName: 'Tony',
+                lastName: 'Stark'
+            },
+            {
+                firstName: 'bruce',
+                lastName: 'Branner'
+            },
+            {
+                firstName: 'Thonr',
+                lastName: 'Odinson'
+            },
+            {
+                firstName: 'Peter',
+                lastName: 'Parker'
+            },
+            {
+                firstName: 'Natasha',
+                lastName: 'Romanof'
+            }
+        ]
+     }
+    }
     userToAloha() {
+        const {usersArray} = this.state; 
         return usersArray.map((user) => <Aloha name={user.firstName}lastName={user.lastName}/>)
+    }
+
+    sortUsers() {
+        const {usersArray} = this.state; 
+        const newArray = [...usersArray];
+        
+        newArray.sort((a, b) => {
+            if(a.firstName > b.firstName) {
+                return 1;
+            } else {
+                return -1
+            }
+        });
+        
+        this.setState({
+            usersArray: newArray
+        })
     }
 
     render() {
         return  (
             <React.Fragment>
+                <button onClick={() => {this.sortUsers}}> sort </button>
                {this.userToAloha}
            </React.Fragment>
         );
